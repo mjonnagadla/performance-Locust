7eb75ace95e4ad2c27117703f35898c504b54c44
mjonnagadla <manasa.jonnagadla@realpage.com>
2022-05-10 19:41:37 +0530

    Added few more API requests

--- api/apiRequest.py
+++ api/apiRequest.py
@@ -2,7 +2,13 @@ import json
 
 
 def getRequestData(self, apiCall):
-    apiRequest = self.client.get(apiCall)
+    headerCollection = {    
+    'x-propertyware-system-id':  '182255624',
+    'x-propertyware-client-id': '0af3cd28-fec2-4db0-87fb-26fa10b3671e',
+    'x-propertyware-client-secret': 'b033363a-f97f-4957-9839-9eb5c28ed9ff'
+    }
+    apiRequest = self.client.get(apiCall, headers=headerCollection)
+    print(apiRequest)
     apiData = apiRequest.text
     parse_json = json.loads(apiData)
 
