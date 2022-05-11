02ead81cf1023ca07a147b7f8b8f07d4b4328ea2
srihithasunkara13 <srihitha.sunkara@realpage.com>
2022-05-11 18:49:31 +0530

    Added csv program

---
+++ groupfile.py
@@ -0,0 +1,28 @@
+import csv
+from locust import TaskSet, task, HttpUser, constant, task;
+
+auth_header = {
+    'x-propertyware-system-id':  '182255624',
+    'x-propertyware-client-id': '0af3cd28-fec2-4db0-87fb-26fa10b3671e',
+    'x-propertyware-client-secret': 'b033363a-f97f-4957-9839-9eb5c28ed9ff'
+    }
+
+class Building(TaskSet):
+    
+    @task(1)
+    def get_buildingId(self):
+        file_handle=open("buildings.csv","r")
+        csv_reader=csv.reader(file_handle)
+
+        for i in file_handle:
+            response = self.client.request(
+            method='GET',
+            url='/pw/api/rest/v1/buildings/'+i,
+            headers=auth_header
+        )
+        print("Buildingid",response,i)
+
+class LoggedInUser(HttpUser):
+    host = "https://sat.propertyware.com"
+    tasks = [Building]
+    wait_time = constant(2)
